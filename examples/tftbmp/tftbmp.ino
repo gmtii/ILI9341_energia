0b4d3ea25927bc3e611fa68ca97b15b90addb0e0
gmtii <estebanjbs@gmail.com>
2013-08-17 17:16:38 +0100

    Update tftbmp.ino
    Added SD support based on FatFs Energia library

--- examples/tftbmp/tftbmp.ino
+++ examples/tftbmp/tftbmp.ino
@@ -1,82 +1,186 @@
-#include <SD.h>
-#include <SPI.h>
+#include "SPI.h" 
 #include "TFTv2.h"
-File bmpFile;
+#include "fatfs.h"
 
-unsigned char saved_spimode;
 int bmpWidth, bmpHeight;
 uint8_t bmpDepth, bmpImageoffset;
 
-#define chipSelect 4
+#define cs_pin      12             		// chip select pin on the arduino
+#define file        "FLOWER.BMP"                // BMP filename on SD
+#define file2       "hibiscus.bmp"              // BMP filename on SD
 
-//set up variables using the SD utility library functions:
-Sd2Card card;
-SdVolume volume;
-SdFile root;
+int rc;
+uint32_t offset_counter=0;                      // Global offset when reading from SD
+
 
 void setup()
 {
-  pinMode(11,INPUT);
-  pinMode(12,INPUT);
-  pinMode(13,INPUT);
-  TFT_CS_HIGH;
-  pinMode(chipSelect,OUTPUT);
-  digitalWrite(chipSelect,HIGH);
 
-  Serial.begin(38400);
-  SPI.begin(); 
-  Tft.TFTinit();
-  //SPI.setClockDivider(SPI_CLOCK_DIV4);
-  //SDcard_info();
-  /**/
-  DDRB |= 0x04;
-  card.init(SPI_FULL_SPEED,chipSelect);//SPI_QUARTER_SPEED   SPI_HALF_SPEED, SPI_FULL_SPEED,
-  if(!SD.begin(chipSelect))//SPI_QUARTER_SPEED,
-  { //53 is used as chip select pin
-    Serial.println("failed!");
-    while(1);
-  }
-  Serial.println("SD OK!");
+  Serial.begin(115200);			   	 // Don't remove!
+
+  Tft.begin(PE_1,PE_2,PE_3,PE_4);                // CS,DC,BL,RESET pin
+  Tft.TFTinit();                                 // init TFT library
+  Tft.backlight_on();                            // turn on the background light
+
+  Tft.drawString("LCD init OK",0,0,2,WHITE,BLACK);  // Init ok message on LCD
+
+  delay(1000);
 
   Tft.setCol(0,239);
   Tft.setPage(0,319);
-  Tft.sendCMD(0x2c);//start to write to display ram
-  TFT_BL_ON;
+  Tft.sendCMD(0x2c);                             // LCD init
+
+  FatFs.begin(cs_pin,2,0);             		 // initialize FatFS library calls on SPI2, SS2=pin 12=PA_5
+
+
+}
+
+void die (int pff_err) 		/* Stop with dying message */
+/* FatFs return value */
+{
+  Serial.println();
+  Serial.print("Failed with rc=");
+  Serial.print(pff_err,DEC);
+  for (;;) ;
+}
+
+void dir_list (void) {
+  FILINFO file_info;            // information on file size and type
+  unsigned int files_found = 0; // count of files found in directory
+
+  FatFs.opendir();
+  do {
+    FatFs.readdir(&file_info);
+
+    if ((file_info.fattrib & AM_DIR) != AM_DIR) {
+      Serial.println(file_info.fname);
+      files_found++;
+      delay(100);
+    }
+  }
+  while (file_info.fname[0] != 0);
+
+  Serial.print(files_found); 
+  Serial.println(" files found.");
+}
+
+// Returns byte located at offset_counter variable and does offset_counter++ for next time
+
+char file_secuential_read() {
+
+  unsigned int bytes_read;                      // number of bytes read each call
+  char out[1];
+
+  FatFs.lseek(offset_counter);                  // set file r/w pointer back to 0
+
+  FatFs.read(out, 1, &bytes_read);
+  offset_counter++;
+
+  return out[0];
 }
 
 void loop()
 {
-  /*
-   */
-  char bmpfiles[][18]=
-  {
-    "123.bmp","hibiscus.bmp","test.bmp"
- };
-  for(unsigned char i=0;i<3;i++)
-  {
-    //TFT_BL_OFF;
-    bmpFile = SD.open(bmpfiles[i]);
-    if (! bmpFile) 
-    {
-      Serial.println("didnt find image");
-      while (1);
-    }
+  dir_list();                                  // debug files on sd to serial
+  rc= FatFs.open(file, FA_READ );              // open file read mode
+  if (rc) die(rc);
 
-    if (! bmpReadHeader(bmpFile)) { 
-      Serial.println("bad bmp");
-      return;
-    }
+  bmpReadHeader();                             // debug bmp header to serial
 
-    Serial.print("image size "); 
-    Serial.print(bmpWidth, DEC);
-    Serial.print(", ");
-    Serial.println(bmpHeight, DEC);
-    bmpdraw(bmpFile, 0, 0);
-    bmpFile.close();
-    //TFT_BL_ON;
-    delay(1000);
-  }
+  bmpdraw(0, 0);                               // draws bmp on screen
+  
+  FatFs.close();                               // closes file
+  
+  offset_counter=0;                            // restarts offset counter for next image
+  
+  delay(5000);
+  
+  rc= FatFs.open(file2, FA_READ );              // open file read mode
+  if (rc) die(rc);
+
+  bmpReadHeader();                             // debug bmp header to serial
+
+  bmpdraw(0, 0);                               // draws bmp on screen
+  
+  FatFs.close();                               // closes file
+
+  for (;;) ;                                   // end
+    
 }
+
+void bmpReadHeader() {
+
+  uint32_t tmp;
+
+  if (read16() != 0x4D42) {
+    // magic bytes missing
+    Serial.print("\nBad header ");
+    return;
+  }
+  else
+    Serial.print("\nBMP file detected ");
+
+  Serial.print("\nSize "); 
+  Serial.print(read32(), DEC); 
+  Serial.println(" bytes.");
+  // read and ignore creator bytes
+  read32();
+
+  bmpImageoffset = read32();  
+  Serial.print("offset "); 
+  Serial.println(bmpImageoffset, DEC);
+
+  // read DIB header
+  tmp = read32();
+  Serial.print("header size "); 
+  Serial.println(tmp, DEC);
+  bmpWidth = read32();
+  bmpHeight = read32();
+
+
+  if (read16() != 1)
+    return ;
+
+  bmpDepth = read16();
+  Serial.print("bitdepth "); 
+  Serial.println(bmpDepth, DEC);
+
+  if (read32() != 0) {
+    // compression not supported!
+    return;
+  }
+
+  Serial.print("compression "); 
+  Serial.println(tmp, DEC);
+
+}
+
+// LITTLE ENDIAN!
+
+uint16_t read16() 
+{
+  uint16_t d;
+  uint8_t b;
+  b = file_secuential_read();
+  d = file_secuential_read();
+  d <<= 8;
+  d |= b;
+  return d;
+}
+
+// LITTLE ENDIAN!
+uint32_t read32() 
+{
+  uint32_t d;
+  uint16_t b;
+
+  b = read16();
+  d = read16();
+  d <<= 16;
+  d |= b;
+  return d;
+}
+
 
 /*********************************************/
 // This procedure reads a bitmap and draws it to the screen
@@ -88,9 +192,12 @@ void loop()
 
 #define BUFFPIXEL 20
 
-void bmpdraw(File f, int x, int y) 
+void bmpdraw(int x, int y) 
 {
-  bmpFile.seek(bmpImageoffset);
+
+  unsigned int bytes_read;         // number of bytes read each call
+
+  FatFs.lseek(bmpImageoffset);
 
   uint32_t time = millis();
   uint16_t p;
@@ -109,7 +216,7 @@ void bmpdraw(File f, int x, int y)
       // read more pixels
       if (buffidx >= 3*BUFFPIXEL) 
       {
-        bmpFile.read(sdbuffer, 3*BUFFPIXEL);
+        FatFs.read(sdbuffer, 3*BUFFPIXEL, &bytes_read);
         buffidx = 0;
       }
 
@@ -136,78 +243,4 @@ void bmpdraw(File f, int x, int y)
   Serial.println(" ms");
 }
 
-boolean bmpReadHeader(File f) {
-  // read header
-  uint32_t tmp;
 
-  if (read16(f) != 0x4D42) {
-    // magic bytes missing
-    return false;
-  }
-
-  // read file size
-  tmp = read32(f);  
-  Serial.print("size 0x"); 
-  Serial.println(tmp, HEX);
-
-  // read and ignore creator bytes
-  read32(f);
-
-  bmpImageoffset = read32(f);  
-  Serial.print("offset "); 
-  Serial.println(bmpImageoffset, DEC);
-
-  // read DIB header
-  tmp = read32(f);
-  Serial.print("header size "); 
-  Serial.println(tmp, DEC);
-  bmpWidth = read32(f);
-  bmpHeight = read32(f);
-
-
-  if (read16(f) != 1)
-    return false;
-
-  bmpDepth = read16(f);
-  Serial.print("bitdepth "); 
-  Serial.println(bmpDepth, DEC);
-
-  if (read32(f) != 0) {
-    // compression not supported!
-    return false;
-  }
-
-  Serial.print("compression "); 
-  Serial.println(tmp, DEC);
-
-  return true;
-}
-
-/*********************************************/
-// These read data from the SD card file and convert them to big endian 
-// (the data is stored in little endian format!)
-
-// LITTLE ENDIAN!
-uint16_t read16(File f) 
-{
-  uint16_t d;
-  uint8_t b;
-  b = f.read();
-  d = f.read();
-  d <<= 8;
-  d |= b;
-  return d;
-}
-
-// LITTLE ENDIAN!
-uint32_t read32(File f) 
-{
-  uint32_t d;
-  uint16_t b;
-
-  b = read16(f);
-  d = read16(f);
-  d <<= 16;
-  d |= b;
-  return d;
-}
